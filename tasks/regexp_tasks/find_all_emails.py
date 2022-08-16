3e931087915bf77868a23a7a785594ddcde270b7
Althran <overlord106@gmail.com>
2022-08-16 17:31:52 +0300

    Complete find_all_emails

--- tasks/regexp_tasks/find_all_emails.py
+++ tasks/regexp_tasks/find_all_emails.py
@@ -2,3 +2,12 @@
 Написать функцию find_emails, которая принимает текст и находит в нем
 все email вида some@some.some
 """
+import re
+
+
+def find_emails(text):
+    pattern = re.compile(r'^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$')
+    text1 = text.split()
+    for i in text1:
+        if pattern.match(i):
+            print(i)
