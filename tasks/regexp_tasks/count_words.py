7e0e2e01fcc5c5842ed0bee593f6c88195fc75b9
Althran <overlord106@gmail.com>
2022-08-16 17:39:59 +0300

    Complete count_words

--- tasks/regexp_tasks/count_words.py
+++ tasks/regexp_tasks/count_words.py
@@ -3,3 +3,14 @@
 с учетом, что в начале могут быть пробелы, в конце могут быть пробелы, между слов
 может встречаться больше одного пробела подряд.
 """
+import re
+
+
+def count_words(text):
+    pattern = re.compile(r'\w+(-\w+)*')
+    count = 0
+    text1 = text.split()
+    for i in text1:
+        if pattern.match(i):
+            count += 1
+    return count
