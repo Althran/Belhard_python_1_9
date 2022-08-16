a15adaf6a5c5ec6313d1da5abb77b537a7c33a83
Althran <overlord106@gmail.com>
2022-08-16 19:28:05 +0300

    Complete enum_tasks

--- tasks/enum_tasks/film_types.py
+++ tasks/enum_tasks/film_types.py
@@ -2,3 +2,17 @@
 Написать класс FilmTypes, который будет наследоваться от Enum и содержать
 константы-названия жанров фильмов.
 """
+import enum
+
+
+class FilmTypes(enum.Enum):
+    ROMANCE_COMEDY = 'Romance_comedy'
+    SCIENCE_FICTION = 'Science_fiction'
+    HORROR = 'Horror'
+    DOCUMENTARY = 'Documentary'
+    ANIMATED = 'Animated'
+    ANIMATION = 'Animation'
+    THRILLER = 'Thriller'
+    DRAMA = 'Drama'
+    COMEDY = 'Comedy'
+    ADVENTURE = 'Adventure'
--- tasks/enum_tasks/month.py
+++ tasks/enum_tasks/month.py
@@ -2,3 +2,19 @@
 Написать класс Month, который будет наследоваться от Enum и содержать
 константы-названия месяцев на английском языке
 """
+import enum
+
+
+class Month(enum.Enum):
+    JANUARY = 'January'
+    FEBRUARY = 'February'
+    MARCH = 'March'
+    APRIL = 'April'
+    MAY = 'May'
+    JUNE = 'June'
+    JULY = 'July'
+    AUGUST = 'August'
+    SEPTEMBER = 'September'
+    OCTOBER = 'October'
+    NOVEMBER = 'November'
+    DECEMBER = 'December'
