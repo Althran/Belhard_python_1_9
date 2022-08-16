9b911258cb2d34c28960371e0c08c409f38e28ad
Althran <overlord106@gmail.com>
2022-08-16 12:11:58 +0300

    time_rotate_logger wip

--- tasks/logging_tasks/time_rotate_logger.py
+++ tasks/logging_tasks/time_rotate_logger.py
@@ -5,3 +5,9 @@
 Настроить, чтобы вывод шел в файл и в консоль.
 Настроить ротацию файла лога по времени (полночь).
 """
+import logging
+from logging.handlers import TimedRotatingFileHandler
+
+logging.basicConfig(level=logging.DEBUG, filename='app.log', filemode='a',
+                    format='%(name)s - %(levelname)s - %(message)s')
+handler = TimedRotatingFileHandler(filename='app.log', when="midnight")
